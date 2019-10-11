411e1be4a6edc80a92b0330f6149b0c214a4f8c6
chenshuhui <55970527+chenshuhui7@users.noreply.github.com>
2019-10-11 18:33:01 +0800

    Create e3-3.c

---
+++ e3-3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+   int no=-8;
+
+   printf("请输入一个整数:%d",no);
+
+
+   if(no>=0)
+    printf("绝对值是%d",no);
+
+    else
+    
+    printf("绝对值是%d",-no);
+    
+  return 0;
+}
