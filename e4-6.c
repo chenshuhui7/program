9112de3fffa702dc06a350d4f6ae814f29241959
chenshuhui <55970527+chenshuhui7@users.noreply.github.com>
2019-10-16 23:36:57 +0800

    Create e4-6.c

---
+++ e4-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)
+{
+    int i=0,no;
+    printf("请输入一个整数：");
+    scanf("%d",&no);
+    while(i<(no-2))
+    {i=i+2;
+
+    printf("%d  ",i);}
+   
+    return 0;
+
+
+}
