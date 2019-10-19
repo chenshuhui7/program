a0728a490de3036c048458ae71d8edae6a4a16ea
chenshuhui <55970527+chenshuhui7@users.noreply.github.com>
2019-10-19 18:53:27 +0800

    Create e4-15.c

---
+++ e4-15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+    int a,b,c;
+    printf("开始数值："); scanf("%d",&a);
+    printf("结束数值："); scanf("%d",&b);
+    printf("间隔数值："); scanf("%d",&c);
+    int sw;
+    for(sw=a;sw<b;sw+=c)
+    {printf("%dcm   ",sw);
+     double se;
+     se=(sw-80)*0.7;
+    printf("  %.2fkg\n",se); 
+}
+
+return 0;
+}
