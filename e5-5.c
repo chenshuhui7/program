385258b0dfb221f725e47df738e0c44755f08362
chenshuhui <55970527+chenshuhui7@users.noreply.github.com>
2019-10-21 23:56:27 +0800

    Create e5-5.c

---
+++ e5-5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#define number 7
+int main(void)
+{
+  int i;
+  int x[number];
+  for(i=0;i<number;i++){
+      printf("x[%d]:  ",i);
+      scanf("%d",&x[i]);
+  }
+    for(i=0;i<(number-1)/2;i++){
+    int temp=x[i];
+    x[i]=x[number-1-i];
+    x[number-1-i]=temp;
+
+}    puts("倒序排列了。");
+   for(i=0;i<number;i++)
+
+  printf("x[%d]=%d\n",i,x[i]);
+   return 0;
+
+
+}
